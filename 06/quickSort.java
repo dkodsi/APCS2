cb2b922fd84d5c071aad8a0b81fc123b8d8f38c4
dkodsi <danielkodsi@gmail.com>
2015-03-17 13:15:57 -0400

    Update quickSort.java

--- 06/quickSort.java
+++ 06/quickSort.java
@@ -1,92 +1,54 @@
 import java.util.*;
 public class quickSort{
 
-  public static void quicksort(int [] arr) {
-	  partition(arr, 0, arr.length-1);
-  }
-  
-  public static void partition(int[] L, int start, int end) {
-	  if (start == end)
-	    return;
-	  int[] other = new int[L.length];
-	  for(int i = 0; i < L.length; i++)
-	    if(i < start || i > end)
-		    other[i] = L[i];
-	}
-
-	Random r = new Random();
-	int pivotindex = r.nextInt(end - start + 1) + start;
-	int min = start;
-	int max = end;
-
-	for(int i = start; i <= end; i++) {
-	    if(L[i] < L[pivotindex]) {
-		    other[min] = L[i];
-		    min++;
-	    } else if(L[i] > L[pivotindex]) {
-		      other[max] = L[i];
-		      max--;
-	    }
-	}
-	other[start] = L[pivotindex];
-	for(int i = 0; i < L.length; i++) {
-	    L[i] = other[i];
-	}
-	partition(L, start, pivotindex-1);
-	partition(L, pivotindex+1, end);
-    }
-    /*Quickselect*/
-    public static int quickselect(int[] array, int index){
-	return qhelp(array, 0, array.length - 1, index);
+    public static String name() {
+	return "daniel.kodsi";
     }
 
-    public static int qhelp(int[] L, int start, int end,int index){
-	if (start == end){
-	    return L[start];
-	}
-	int[] other = new int[L.length];
-	for(int i = 0; i < L.length; i++){
-	    if(i < start || i > end){
-		other[i] = L[i];
-	    }
-	}
+    public static void swap(int[] List, int a, int b) {
+	int temp = List[a];
+	List[a] = List[b];
+	List[b] = temp;
+    }
 
+    public static int partition(int[] List, int start, int end) {
 	Random r = new Random();
-	int pivotindex = r.nextInt(end - start + 1) + start;
-	int min = start;
-	int max = end;
-
-	for(int i = start; i <= end; i++){
-	    if(L[i] < L[pivotindex]){
-		other[min] = L[i];
-		min++;
-	    }else if(L[i] > L[pivotindex]){
-		other[max] = L[i];
-		max--;
-	    }
-	}
-	other[min] = L[pivotindex];
-
-	if(min == index){
-	    return other[min];
-	}
-	for(int i = 0; i < L.length; i++){
-	    L[i] = other[i];
-	}
-	if(min > index) {
-	    return qhelp(L,start, min - 1, index);
-	}
+	int pivotIndex = r.nextInt(end - start + 1) + start;
+	int pivot = List[pivotIndex];
+	int count = start;
+	int end2 = end;
+	swap(List,end2,pivotindex);
 	
-	return qhelp(L, min + 1, end, index);
+	for(int i = count; i < end; i++) {
+	    if(List[end2] > List[i]){
+		swap(L,count,i);
+		count++;
+	    }
+	}
+	swap(List,end2,count);
+	return count;
     }
 
-  public static void main(String[] args) {
-	  int[] bleh = {6,2,8,4,5,7,13,200,52};
-	  System.out.println(Arrays.toString(bleh));
-	  System.out.println(quickselect(bleh,5));
-	  quicksort(bleh);
-	  System.out.println(Arrays.toString(bleh));
-  }
+    public static void quickSortHelper(int[] List, int start, int end) {
+    	if (start >= end)	
+		return;
+	int pivotIndex = partition(List,start,end);
+	System.out.println(pivotIndex);
+	quickSortHeper(List, start, pivotIndex);
+	quickSortHelper(List, pivotindex + 1, end);
+    }
 
+    public static void quickSort(int[] List) {
+	quickSortHelper(List, 0, List.length-1);
+    }
+
+    public static void main(String[] args) {
+	int[] List = new int[5];
+	Random r = new Random();
+	for (int i = 0; i < List.length; i++)
+	    List[i] = 0;
+	System.out.println(Arrays.toString(List));
+	quickSort(List);
+	System.out.println(Arrays.toString(List));
+    }
 }
-
